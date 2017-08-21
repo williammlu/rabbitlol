2a54ff1a1faf1b4010390efd6a4e0d8db1ce1b2d
William Lu <wml@berkeley.edu>
2017-08-21 02:29:26 -0700

    Fix start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
-virtualenv . && source bin/activate && pip install coverage flask mock nose-exclude nosegae oauth2 pytest pyyaml
+virtualenv . && pip install coverage flask mock nose-exclude nosegae oauth2 pytest pyyaml
+
+source bin/activate
 
 ls app/secrets.py || cp app/secrets.py_TEMPLATE app/secrets.py
 
